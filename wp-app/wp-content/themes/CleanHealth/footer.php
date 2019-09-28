0ddbbcb1c5688d7e46bc5d7cf90eed5bb5fcc203
hengxheng <hengxheng@hotmail.com>
2019-09-28 15:44:28 +1000

    response footer

--- wp-app/wp-content/themes/CleanHealth/footer.php
+++ wp-app/wp-content/themes/CleanHealth/footer.php
@@ -27,13 +27,13 @@
 			</div>
 			<nav class="footer-navigation" aria-label="Footer Menu">
 				<?php
-				wp_nav_menu(
-					array(
-						'theme_location' => 'footer',
-						'menu_class'     => 'footer-menu',
-						'depth'          => 1,
-					)
-				);
+					wp_nav_menu(
+						array(
+							'theme_location' => 'footer',
+							'menu_class'     => 'footer-menu',
+							'depth'          => 1,
+						)
+					);
 				?>
 			</nav>
 		</div>
@@ -52,6 +52,25 @@
 			</div>
 		</div>
 			</div>
+	</div>
+	<div class="footer-mobile-top">
+		<div class="fm-social">
+			<?php echo do_shortcode('[widget id="custom_html-4"]'); ?>
+		</div>
+		<div class="fm-footer-menu">
+			<?php
+				wp_nav_menu(
+					array(
+						'menu' => 'mobile-footer',
+						'menu_class'     => 'footer-mobile-menu',
+						'depth'          => 1,
+					)
+				);
+			?>
+		</div>
+		<div class="footer-rto">
+			RTO Provider Number: 40538
+		</div>
 	</div>
 	<div class="footer-bottom">
 		<div class="content-inner withPadding">
