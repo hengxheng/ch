7ec470bf7880e4eff2846a2a732489c0ab8efe42
hengxheng <hengxheng@hotmail.com>
2019-10-09 22:46:59 +1100

    checkout page

--- wp-app/wp-content/themes/CleanHealth/woocommerce/checkout/form-checkout.php
+++ wp-app/wp-content/themes/CleanHealth/woocommerce/checkout/form-checkout.php
@@ -18,7 +18,9 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-
+?>
+<div class="content-inner withPadding">
+<?php
 do_action( 'woocommerce_before_checkout_form', $checkout );
 
 // If checkout registration is disabled and not logged in, the user cannot checkout.
@@ -41,6 +43,9 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 			</div>
 
 			<div class="col-2">
+			<div id="order_review" class="woocommerce-checkout-review-order">
+				<?php do_action( 'woocommerce_checkout_order_review' ); ?>
+			</div>
 				<?php do_action( 'woocommerce_checkout_shipping' ); ?>
 			</div>
 		</div>
@@ -55,12 +60,11 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 	
 	<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
 
-	<div id="order_review" class="woocommerce-checkout-review-order">
-		<?php do_action( 'woocommerce_checkout_order_review' ); ?>
-	</div>
+	
 
 	<?php do_action( 'woocommerce_checkout_after_order_review' ); ?>
 
 </form>
 
 <?php do_action( 'woocommerce_after_checkout_form', $checkout ); ?>
+</div>
--- wp-app/wp-content/themes/CleanHealth/woocommerce/checkout/form-coupon.php
+++ wp-app/wp-content/themes/CleanHealth/woocommerce/checkout/form-coupon.php
@@ -22,21 +22,14 @@ if ( ! wc_coupons_enabled() ) { // @codingStandardsIgnoreLine.
 }
 
 ?>
-<div class="woocommerce-form-coupon-toggle">
-	<?php wc_print_notice( apply_filters( 'woocommerce_checkout_coupon_message', esc_html__( 'Have a coupon?', 'woocommerce' ) . ' <a href="#" class="showcoupon">' . esc_html__( 'Click here to enter your code', 'woocommerce' ) . '</a>' ), 'notice' ); ?>
-</div>
 
-<form class="checkout_coupon woocommerce-form-coupon" method="post" style="display:none">
-
-	<p><?php esc_html_e( 'If you have a coupon code, please apply it below.', 'woocommerce' ); ?></p>
-
-	<p class="form-row form-row-first">
-		<input type="text" name="coupon_code" class="input-text" placeholder="<?php esc_attr_e( 'Coupon code', 'woocommerce' ); ?>" id="coupon_code" value="" />
-	</p>
-
-	<p class="form-row form-row-last">
-		<button type="submit" class="button" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?>"><?php esc_html_e( 'Apply coupon', 'woocommerce' ); ?></button>
-	</p>
-
-	<div class="clear"></div>
+<form class="checkoutCoupon" method="post">
+	<div><span>Enter your coupon code if you have one</span></div>
+	<div>
+		<input type="text" name="coupon_code" class="input-text" placeholder="Type your coupon code here" id="coupon_code" value="" />
+	</div>
+	<div>
+		<button type="submit" class="g-btn" name="apply_coupon" value="APPLY COUPON">APPLY COUPON</button>
+	</div>
 </form>
+<div class="clear"></div>
